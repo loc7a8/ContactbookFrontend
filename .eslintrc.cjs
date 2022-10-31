7e5811ad2f9879fa342e6478bff4313616c85ac0
loc7a8 <locb1910664@student.ctu.edu.vn>
2022-10-31 09:01:58 +0700

    Init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
+
+  rules: {
+    'vue/no-reserved-component-names': 'off',
+    },
+
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+
+
+
+}
